18a112f88cf414846513a8e2cf173eee8091c768
Takuro Tsujikawa <40074491+takurot@users.noreply.github.com>
2026-04-04 12:08:52 +0900

    feat: add src/renderer.rs

---
+++ src/renderer.rs
@@ -0,0 +1,46 @@
+use crate::config::Config;
+use crate::state::State;
+use crate::ui::{self};
+use crate::util::Result;
+
+use crossterm::terminal::{self};
+use crossterm::{ExecutableCommand};
+
+use tui::{Terminal};
+use tui::backend::{CrosstermBackend};
+
+use std::io::Write;
+
+pub struct Renderer<W: Write> {
+    terminal: Terminal<CrosstermBackend<W>>,
+}
+
+impl<W: Write> Renderer<W> {
+    pub fn new(mut out: W) -> Result<Renderer<W>> {
+        terminal::enable_raw_mode()?;
+        out.execute(terminal::EnterAlternateScreen)?;
+
+        Ok(Renderer { terminal: Terminal::new(CrosstermBackend::new(out))? })
+    }
+
+    pub fn render(&mut self, state: &State, config: &Config) -> Result<()> {
+        self.terminal
+            .draw(|frame| ui::draw(frame, state, frame.size(), &config.theme, &config.language))?;
+        Ok(())
+    }
+}
+
+impl<W: Write> Drop for Renderer<W> {
+    fn drop(&mut self) {
+        self.terminal
+            .backend_mut()
+            .execute(terminal::LeaveAlternateScreen)
+            .expect("Could not execute to stdout");
+        terminal::disable_raw_mode().expect("Terminal doesn't support to disable raw mode");
+        if std::thread::panicking() {
+            eprintln!(
+                "triadchat panicked, redirect stderr to a file to inspect the failure, for example: triadchat 2> triadchat.log",
+            );
+        }
+    }
+}
